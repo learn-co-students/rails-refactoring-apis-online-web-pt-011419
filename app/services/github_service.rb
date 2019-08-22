0e44dae8b6ea16d13d0c8458e7e2cc91a824c098
dclements9 <dclements@protonmail.com>
2019-08-22 16:57:11 -0400

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,10 @@
 class GithubService
 
+    # here
+    def initialize(access_hash = nil)
+        @access_token = access_hash['access_token'] if access_hash
+    end
+
     def authenticate!(client_id, client_secret, code)
         resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
             req.params['client_id'] = client_id
